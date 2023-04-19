23f58d869571f4000d51d0dfd719079e9aca8386
Nikita <100973527+xaritoshaxnx@users.noreply.github.com>
2023-04-19 17:44:23 +0300

    added comments

--- noted/Noted/mainwindow.h
+++ noted/Noted/mainwindow.h
@@ -20,17 +20,17 @@ namespace Ui {
 class MainWindow;
 }
 
-class User
+class User                                                           // класс представляющий пользователя
 {
 private:
-    int id;
-    QString strId;
-    QString mail;
-    QString password;
-    QString image;
+    int id;                                                          // уникальный идентификатор пользователя
+    QString strId;                                                   // строчное представления id
+    QString mail;                                                    // логин пользователя
+    QString password;                                                // пароль пользователя
+    QString image;                                                   // изображение профиля пользователя
 
 public:
-    User(QSqlRecord rec)
+    User(QSqlRecord rec)                                             // функция доступа к данным
     {
         id = rec.value("id").toInt();
         strId = rec.value("id").toString();
@@ -58,91 +58,91 @@ public:
 private slots:
 
     
-    void on_addNote_clicked();
+    void on_addNote_clicked();                                              // отображение формы с заполняемыми полями для создания заметки
 
-    void on_returnButton_clicked();
+    void on_returnButton_clicked();                                         // отмена редактирования заметки
 
-    void on_dayT_clicked();
+    void on_dayT_clicked();                                                 // отображение заметок группы "Daily tasks"
 
-    void on_impT_clicked();
+    void on_impT_clicked();                                                 // отображение заметок группы "Important tasks"
 
-    void on_planT_clicked();
+    void on_planT_clicked();                                                // отображение заметок группы "Planned tasks"
 
-    void on_complTasks_clicked();
+    void on_complTasks_clicked();                                           // отображение заметок группы "Completed tasks"
 
-    void on_boolPlanned_toggled(bool checked);
+    void on_boolPlanned_toggled(bool checked);                              // отображение виджета дата/время
 
-    void on_boolCompleted_toggled(bool checked);
+    void on_boolCompleted_toggled(bool checked);                            // отображение формы для ввода названия новой группы заметок
 
-    void on_addType_clicked();
+    void on_addType_clicked();                                              // отображение формы для редактирования заметки с выводом текущих значений
 
-    void on_noteList_doubleClicked(const QModelIndex &index);
+    void on_noteList_doubleClicked(const QModelIndex &index);               // сохранение информации о заметке после редактирования
 
-    void on_applyButton_clicked();
+    void on_applyButton_clicked();                                          // сохранение информации о заметке после редактирования
 
-    void on_editButton_clicked();
+    void on_editButton_clicked();                                           // отображение полной информации об заметке
 
-    void on_addTypeApply_clicked();
+    void on_addTypeApply_clicked();                                         // добавление новой группы заметок
 
-    void on_addNoteApply_clicked();
+    void on_addNoteApply_clicked();                                         // добавление новой заметки
 
-    void on_loadPhotoButton_clicked();
+    void on_loadPhotoButton_clicked();                                      // изменение изображения профиля
 
-    void on_toolButton_clicked();
+    void on_toolButton_clicked();                                           // отображение меню с функциями сортировки
 
 
-    void on_registrationButton_clicked();
+    void on_registrationButton_clicked();                                   // отображение формы с регистрацией
 
-    void on_authorizationButton_clicked();
+    void on_authorizationButton_clicked();                                  // отображение формы с авторизацией
 
-    void on_authShowPass_stateChanged(int arg1);
+    void on_authShowPass_stateChanged(int arg1);                            // показать/скрыть пароль при авторизации
 
-    void on_regShowPass_stateChanged(int arg1);
+    void on_regShowPass_stateChanged(int arg1);                             // показать/скрыть пароль при регистрации
 
-    void on_loginButton_clicked();
+    void on_loginButton_clicked();                                          // авторизация пользователя
 
-    void on_registerButton_clicked();
+    void on_registerButton_clicked();                                       // регистрация нового пользователя
 
-    void on_btnList1_clicked();
+    void on_btnList1_clicked();                                             // отображение первой группы заметок
+    
+    void on_btnList2_clicked();                                             // отображение второй группы заметок
 
-    void on_btnList2_clicked();
+    void on_btnList3_clicked();                                             // отображение третей группы заметок
 
-    void on_btnList3_clicked();
+    void on_btnList4_clicked();                                             // отображение четвертой группы заметок
 
-    void on_btnList4_clicked();
+    void on_btnList5_clicked();                                             // отображение пятой группы заметок
 
-    void on_btnList5_clicked();
+    void on_btnList6_clicked();                                             // отображение шестой группы заметок
 
-    void on_btnList6_clicked();
+    void on_deleteType_clicked();                                           // удаление выбранной группы заметок
 
-    void on_deleteType_clicked();
+    void btns_click();                                                      // выбор из базы данных заметок выбранной группы
 
-    void btns_click();
+    void on_boolPlanned_stateChanged(int arg1);                             // изменение содержимого
 
-    void on_boolPlanned_stateChanged(int arg1);
+    void on_cancelNote_clicked();                                           // отмена добавления новой заметки
 
-    void on_cancelNote_clicked();
+    void sortByTitle();                                                     // сортировка по заголовку по убыванию
 
-    void sortByTitle();
+    void sortByDateOfCreate();                                              // сортировка по дате создания по возрастанию
 
-    void sortByDateOfCreate();
+    void sortByTitleACK();                                                  // сортировка по заголовку по возрастанию
 
-    void sortByTitleACK();
+    void sortByDateOfCreateDESC();                                          // сортировка по дате создания по убыванию
 
-    void sortByDateOfCreateDESC();
-
-    void on_removeNote_clicked();
+    void on_removeNote_clicked();                                           // удаление заметки
 
 private:
     Ui::MainWindow *ui;
-    QSqlDatabase db;
-    QSqlQuery query;
-    int countOfButtons = 0;
-    QWidget *widg = new QWidget;
-    User loginedUser;
-    QPushButton **listBtn;
-    QPushButton *currentBtn;
-    QString prevStr;
+    QSqlDatabase db;                        //переменная соединения с базой данной
+    QSqlQuery query;                        //переменная представляющая интерфейс для выполнения операций SQL    
+    int countOfButtons = 0;                 //счетчик созданных групп
+    QWidget *widg = new QWidget;            //указатель на виджет для корректного отображения формы с собственными группами
+    User loginedUser;                       //объект класса User, представляющий запись из таблицы 'users'
+    QPushButton **listBtn;                  //список созданных кнопок (кастомных групп)
+    QPushButton *currentBtn;                //текущая выбранная группа
+    QString prevStr;                        //название предыдущей группы
 };
 
 #endif // MAINWINDOW_H
